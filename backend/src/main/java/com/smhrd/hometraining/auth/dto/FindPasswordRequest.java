176649fcf0943fa17fecf633fe18bde4f1147b35
허재석 <jaeseok8214@gmail.com>
2026-09-18 19:30:25 +0900

    Delete backend directory

--- backend/src/main/java/com/smhrd/hometraining/auth/dto/FindPasswordRequest.java
+++
@@ -1,9 +0,0 @@
-package com.smhrd.hometraining.auth.dto;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record FindPasswordRequest(
-        @NotBlank String loginId,
-        @NotBlank @Email String email
-) {}
--- backend/src/main/java/com/smhrd/hometraining/shop/repository/ShopItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.smhrd.hometraining.shop.repository;
-
-import com.smhrd.hometraining.shop.entity.ShopItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopItemRepository extends JpaRepository<ShopItem, Long> {
-    boolean existsByName(String name);
-}
